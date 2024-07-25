58a3ab3d7d345e4edd47e83f2760463020b1fd79
Fenilpatel4 <patelfenil22062005@gmail.com>
2024-07-25 23:14:50 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("My self Fenil Nimeshbhai patel.\n");
+	printf("I am 19 year old.\n");
+	printf("I am currently pursuing my bachlor's in information technology in shanti business school.\n");
+	printf("I have intrest in technology and artificial intelligence.\n");
+	printf("I love to play kabbadi,cricket and online mobiles games.\n");
+}
+
+	
